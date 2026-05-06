740e295a4543dfb9e515eb742385c68d1d80e962
Rashi Karande <rashikarande2214@gmail.com>
2026-05-06 17:27:16 +0530

    Moved API client inside function

--- core/utils/gemini_service.py
+++ core/utils/gemini_service.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Groq(api_key=os.getenv('GROQ_API_KEY'))
+def extract_action_plan(text):
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def extract_action_plan(raw_text):
     prompt = f"""
